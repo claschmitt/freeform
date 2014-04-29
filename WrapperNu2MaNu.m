f2ff7dc71ab45919a8eefa8310b56ad7978b64de
claschmitt <claudius.schmitt@tuwien.ac.at>
2014-04-29 15:26:56 +0200

    bug correction

--- WrapperNu2MaNu.m
+++ WrapperNu2MaNu.m
@@ -6,13 +6,21 @@ if isequal(nurbs.form,'B-Spline-Surface')
     MatLabNurbs.dim(1,2)     = nurbs.dimV;
     MatLabNurbs.number(1,1)  = nurbs.numberU;
     MatLabNurbs.number(1,2)  = nurbs.numberV;
-    MatLabNurbs.coefs(1,:,:) = reshape(nurbs.coefs(1,:),nurbs.numberU,nurbs.numberV);
-    MatLabNurbs.coefs(2,:,:) = reshape(nurbs.coefs(2,:),nurbs.numberU,nurbs.numberV);
-    MatLabNurbs.coefs(3,:,:) = reshape(nurbs.coefs(3,:),nurbs.numberU,nurbs.numberV);
+    [numRowsCoefs numColsCoefs] = size(nurbs.coefs);
+    if numRowsCoefs > numColsCoefs
+        MatLabNurbs.coefs(1,:,:) = reshape(nurbs.coefs(:,1),nurbs.numberU,nurbs.numberV);
+        MatLabNurbs.coefs(2,:,:) = reshape(nurbs.coefs(:,2),nurbs.numberU,nurbs.numberV);
+        MatLabNurbs.coefs(3,:,:) = reshape(nurbs.coefs(:,3),nurbs.numberU,nurbs.numberV);
+    else
+        MatLabNurbs.coefs(1,:,:) = reshape(nurbs.coefs(:,1),nurbs.numberU,nurbs.numberV);
+        MatLabNurbs.coefs(2,:,:) = reshape(nurbs.coefs(:,2),nurbs.numberU,nurbs.numberV);
+        MatLabNurbs.coefs(3,:,:) = reshape(nurbs.coefs(:,3),nurbs.numberU,nurbs.numberV);
+    end
     MatLabNurbs.coefs(4,:,:) = ones(nurbs.numberU,nurbs.numberV);
     MatLabNurbs.order(1,1)   = nurbs.orderU;
     MatLabNurbs.order(1,2)   = nurbs.orderV;
     MatLabNurbs.knots{1}     = nurbs.knotsU;
     MatLabNurbs.knots{2}     = nurbs.knotsV;
 else
+    MatLabNurbs = nurbs;
 end
