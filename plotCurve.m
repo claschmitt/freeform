fca35887beae8d2222a2aaa481619399f8e04e8b
claschmitt <claudius.schmitt@tuwien.ac.at>
2014-05-16 16:28:15 +0200

    update

--- plotCurve.m
+++ plotCurve.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% plot curve 
+% plot curve
 %
 % INPUT
 % nurbs = nurbs struct
@@ -8,31 +8,44 @@
 % axesHandle = axes Handle
 % OUTPUT
 % plotting nurbs curve
-% 
+%
 % cs, 22.08.2012
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function subPoints = plotCurve(nurbs,subInterval,figureCurve)
 % subInterval=100;
 
+[r, dimPoints] = size(nurbs.coefs);
 subPosition = linspace(0.0,1.0,subInterval);
-subPoints = zeros(subInterval,3);
+subPoints = zeros(subInterval,dimPoints);
 figure(figureCurve);
-    
+
 for i=1:subInterval
-    subPoints(i,:) = curvePoint(nurbs.numberU -1, nurbs.orderU -1, nurbs.knotsU, nurbs.coefs',subPosition(i));
+    subPoints(i,:) = curvePoint(subPosition(i), nurbs);
 end
 
 % % Plot curve
-% plot3(subPoints(:,1),subPoints(:,2),subPoints(:,3), '-b');  
+% plot3(subPoints(:,1),subPoints(:,2),subPoints(:,3), '-b');
 % hold on;
 % % Plot controle polygon
 % plot3(nurbs.coefs(1,:),nurbs.coefs(2,:), nurbs.coefs(3,:),'--r');
 
-plot(subPoints(:,1),subPoints(:,3), '-b');  
-hold on;
-% Plot controle polygon
-plot(nurbs.coefs(1,:), nurbs.coefs(3,:),'--.r');
-
-hold off;
+switch dimPoints
+    case 2
+        plot(subPoints(:,1),subPoints(:,2), '-b');
+        hold on;
+        % Plot controle polygon
+        plot(nurbs.coefs(:,1), nurbs.coefs(:,2),'--.r');
+        
+        hold off;
+    case 3
+        plot3(subPoints(:,1), subPoints(:,2),subPoints(:,3), '-b');
+        hold on;
+        % Plot controle polygon
+        plot3(nurbs.coefs(:,1), nurbs.coefs(:,2), nurbs.coefs(:,3),'--.r');
+        
+        hold off;
+    otherwise
+        disp('ERROR - points Dimesion')
+end
 
