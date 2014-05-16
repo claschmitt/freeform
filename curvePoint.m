522d3c8be38269f92b0f90477f4bb3493c785e62
claschmitt <claudius.schmitt@tuwien.ac.at>
2014-05-16 16:27:04 +0200

    2D, 3D

--- curvePoint.m
+++ curvePoint.m
@@ -1,32 +1,25 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Compute a point on the Spline 
+%Compute a point on the Spline
 %
 % INPUT
 % n = max index control points
 % p = degree
-% U = knot vektor 
+% U = knot vektor
 % P = ControlPoints
 % u = new Point on the NURBS Value between 0 and 1
 % OUTPUT
 % C = Point on curve
-% 
+%
 % cs, 25.05.2012
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function C = curvePoint(n,p,U,P,u)
+function curvePoint = curvePoint(pointsParameter, nurbs)
 
-if u >= U(p +1 +n-p +1)
-    span = length(U)-p-1 -1;
-else
-    tmp_span = find(U <= u);
-    span = max(tmp_span)-1;
-end
+[r, dimPoints] = size(nurbs.coefs);
+spanU = findSpan(pointsParameter,nurbs.knotsU);
 
-N = basisFunction(span,u,p,U);
-C = [0.0 0.0 0.0];
+coefsReshaped = reshape(nurbs.coefs(spanU-nurbs.orderU +1 : spanU-nurbs.orderU + nurbs.orderU,:)',nurbs.orderU * dimPoints,1);
+AMatrix = fillAGM (zeros(1,dimPoints), pointsParameter, nurbs);
 
-C(1,1) = sum (N(0 +1 : p +1) * P(span-p +1 : span-p+p +1,1));
-C(1,2) = sum (N(0 +1 : p +1) * P(span-p +1 : span-p+p +1,2));
-C(1,3) = sum (N(0 +1 : p +1) * P(span-p +1 : span-p+p +1,3));
-%     plot(C(:,1), C(:,2), 'color', 'red');
+curvePoint = (AMatrix * coefsReshaped)'; 
 
